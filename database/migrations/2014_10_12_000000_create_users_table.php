95cf3d8ca3ee191cd7b7ad66b37d66177599ff8c
channak.chhon <channak.chhon@passerellesnumeriques.org>
2020-06-08 14:27:18 +0700

    modify user migaration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->integer('role')->default(0);//0:normal,1:admin
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password');            
+            $table->unsignedBigInteger('position_id');
+            $table->foreign('position_id')->references('id')->on('positions');
             $table->rememberToken();
             $table->timestamps();
         });
