879729c5641584ed94955495281a56ac7a602f52
channak.chhon <channak.chhon@passerellesnumeriques.org>
2020-06-11 14:20:22 +0700

    Followup List

--- resources/views/followuplist.blade.php
+++ resources/views/followuplist.blade.php
@@ -2,42 +2,45 @@
 <br>
 <div class="row">
     <div class="col-12">
-<table class="table table-bordered table-hover">
-    <thead class="thead-light">
-        <tr>
-            <th style="width:10%">Picture</th>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Class</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody id="myTable">
-        <tr>
-            <td><img src="{{asset('image/student.png')}}" alt="avatar" width="100px" height="100px"></td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-            <td>john@example.com</td>
-            <td><i class="fas fa-user-alt-slash"></i> | <i class="fas fa-user-edit"></i> </td>
-        </tr>
-        <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
-            <td>mary@example.com</td>
-            <td>Out | Edit</td>
-        </tr>
-        <tr>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
-            <td>july@example.com</td>
-            <td>Out | Edit</td>
-        </tr>
-    </tbody>
-</table>
+        <table class="table table-bordered table-hover">
+            <thead class="thead-light">
+                <tr>
+                    <th style="width:10%">Picture</th>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Class</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody id="myTable">
+                <tr onclick="showDetail()">
+                    <td><img src="{{asset('image/student.png')}}" alt="avatar" width="100px" height="100px"></td>
+                    <td>Doe</td>
+                    <td>john@example.com</td>
+                    <td>john@example.com</td>
+                    <td> <a class="text-success" href="#"><i class="fas fa-user-alt-slash"></i></a> | <a href="#"><i class="fas fa-user-edit"></i></a></td>
+                </tr>
+                <tr>
+                    <td>Mary</td>
+                    <td>Moe</td>
+                    <td>mary@example.com</td>
+                    <td>mary@example.com</td>
+                    <td>Out | Edit</td>
+                </tr>
+                <tr>
+                    <td>July</td>
+                    <td>Dooley</td>
+                    <td>july@example.com</td>
+                    <td>july@example.com</td>
+                    <td>Out | Edit</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
-</div>
+
+@include('followupdetail')
+
 @push('scripts')
 <script>
     $(document).ready(function(){
@@ -48,6 +51,11 @@
         });
       });
 
+     
+
     });
+    function showDetail(){
+        $('#detailModal').modal('show')
+    }
 </script>
 @endpush
